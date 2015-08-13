baf560068834ac8a784234b5c4ab75812deb98de
mattans13 <mattans13@gmail.com>
2015-08-13 14:27:29 +0300

    Added second activity - the grid view for displaying results

---
+++ app/src/main/java/com/amirnaveh/itag/GridViewAdapter.java
@@ -0,0 +1,57 @@
+package com.amirnaveh.itag;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by quickode037 on 8/13/15.
+ */
+public class GridViewAdapter extends ArrayAdapter {
+    private Context context;
+    private int layoutResourceId;
+    private ArrayList data = new ArrayList();
+
+    public GridViewAdapter (Context context, int layoutResourceId, ArrayList data){
+        super(context, layoutResourceId, data);
+        this.context = context;
+        this.layoutResourceId = layoutResourceId;
+        this.data = data;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        View row = convertView;
+        ViewHolder holder = null;
+
+        if (row == null){
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+            holder = new ViewHolder();
+            holder.imageTitle = (TextView) row.findViewById(R.id.text);
+            holder.image = (ImageView) row.findViewById(R.id.image);
+            row.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) row.getTag();
+        }
+
+        ImageItem item = data.get(position);
+        holder.imageTitle.setText(item.getTitle());
+        holder.image.setImageBitmap(item.getImage());
+
+        return row;
+    }
+
+    static class ViewHolder {
+        TextView imageTitle;
+        ImageView image;
+    }
+}
