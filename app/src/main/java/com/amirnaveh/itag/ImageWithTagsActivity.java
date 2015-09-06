ef9fd9a2317afc56d58f0c10008fc3ad9124de91
mattans13 <mattans13@gmail.com>
2015-09-06 23:36:18 +0300

    Changes to tags view inside image with tags view

--- app/src/main/java/com/amirnaveh/itag/ImageWithTagsActivity.java
+++ app/src/main/java/com/amirnaveh/itag/ImageWithTagsActivity.java
@@ -2,12 +2,19 @@ package com.amirnaveh.itag;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,18 +24,21 @@ import java.util.ArrayList;
  */
 public class ImageWithTagsActivity extends AppCompatActivity {
 
-    private GridView gridViewTags;
-    private TagGridViewAdapter tagGridViewAdapter;
+    private HorizontalScrollView horizontalScrollViewTags;
+    private LinearLayout linearLayoutInHorizontal;
+//    private TagGridViewAdapter tagGridViewAdapter;
     private ArrayList data;
-    private int widestColumn;
+
+//    private int widestColumn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_with_tags);
 
-        gridViewTags = (GridView) findViewById(R.id.tags_grid_view);
-        tagGridViewAdapter = new TagGridViewAdapter(this, R.layout.tag_grid_view_item, getData());
-        gridViewTags.setAdapter(tagGridViewAdapter);
+        horizontalScrollViewTags = (HorizontalScrollView) findViewById(R.id.HorizontalScrollView_Tags);
+//        tagGridViewAdapter = new TagGridViewAdapter(this, R.layout.tag_grid_view_item, getData());
+//        gridViewTags.setAdapter(tagGridViewAdapter);
+        linearLayoutInHorizontal = (LinearLayout) findViewById(R.id.LinearLayout_In_Horizontal_Layout);
 
         String title = getIntent().getStringExtra("title");
         Bitmap bitmap = getIntent().getParcelableExtra("image");
@@ -36,12 +46,31 @@ public class ImageWithTagsActivity extends AppCompatActivity {
         ImageView imageView = (ImageView) findViewById(R.id.image);
         imageView.setImageBitmap(bitmap);
 
-        if (tagGridViewAdapter.getData().isEmpty()){
-            gridViewTags.setVisibility(View.GONE);
+
+
+//        linearLayout.setDividerDrawable(getResources().getDrawable(R.drawable.divider));
+        for (int i = 0; i < getData().size(); i++) {
+
+            final TextView textView = new TextView(this);
+
+            textView.setTextColor(Color.WHITE);
+            textView.setTextSize(20);
+            textView.setPadding(10,0,10,0);
+            textView.setTag(i);
+            final int j = i;
+            textView.setText("#" + (String) getData().get(i));
+
+
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println(getData().get(j));
+                }
+            });
+
+            linearLayoutInHorizontal.addView(textView);
         }
-        else {
-            gridViewTags.setVisibility(View.VISIBLE);
-        }
+
 
 
 //        if(tags.isEmpty()){
