6336edb0e6722aff4e9f30310216dc0287496312
Amir Naveh <amirnaveh@gmail.com>
2015-09-06 21:01:45 +0300

    Fixed DB to contain keywords in one string

--- app/src/main/java/com/amirnaveh/itag/TagPhotoDbHelper.java
+++ app/src/main/java/com/amirnaveh/itag/TagPhotoDbHelper.java
@@ -27,18 +27,7 @@ public class TagPhotoDbHelper extends SQLiteOpenHelper {
     private static final String COL1 = BaseColumns._ID;
     private static final String COL2 = "file_name"; // full file name (including path)
     //    private static final String COL3 = "path"; // file path
-    private static final String COL3 = "keyword1";
-    private static final String COL4 = "keyword2";
-    private static final String COL5 = "keyword3";
-    private static final String COL6 = "keyword4";
-    private static final String COL7 = "keyword5";
-    private static final String COL8 = "keyword6";
-    private static final String COL9 = "keyword7";
-    private static final String COL10 = "keyword8";
-    private static final String COL11 = "keyword9";
-    private static final String COL12 = "keyword10";
-
-//    private static final int counter = 0;
+    private static final String COL3 = "keywords";
 
     public TagPhotoDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -50,16 +39,7 @@ public class TagPhotoDbHelper extends SQLiteOpenHelper {
         String sql = "CREATE TABLE " + TABLE_NAME + " ( " +
                 COL1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COL2 + " TEXT, " +
-                COL3 + " TEXT, " +
-                COL4 + " TEXT, " +
-                COL5 + " TEXT, " +
-                COL6 + " TEXT, " +
-                COL7 + " TEXT, " +
-                COL8 + " TEXT, " +
-                COL9 + " TEXT, " +
-                COL10 + " TEXT, " +
-                COL11 + " TEXT, " +
-                COL12 + " TEXT)";
+                COL3 + " TEXT)";
 
 
         db.execSQL(sql);
@@ -74,22 +54,23 @@ public class TagPhotoDbHelper extends SQLiteOpenHelper {
 
     }
 
-    public long addTags(String fileName, String[] keywords) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(COL2, fileName); // task name
-        for (int i = 0; i < keywords.length; i++) { // Add keywords to corresponding column
-            values.put("keyword" + Integer.toString((i + 1)), keywords[i]);
-        }
-
-        long id = db.insert(TABLE_NAME, null, values); // Inserting row to db, getting the row id
-
-        db.close(); // Closing database connection
-
-        return id;
-
-    }
+//    TODO REMOVE IF INDEED UNNECESSARY
+//    public long addTags(String fileName, String keywords) {
+//        SQLiteDatabase db = this.getWritableDatabase();
+//
+//        ContentValues values = new ContentValues();
+//        values.put(COL2, fileName); // task name
+//        for (int i = 0; i < keywords.length; i++) { // Add keywords to corresponding column
+//            values.put("keyword" + Integer.toString((i + 1)), keywords[i]);
+//        }
+//
+//        long id = db.insert(TABLE_NAME, null, values); // Inserting row to db, getting the row id
+//
+//        db.close(); // Closing database connection
+//
+//        return id;
+//
+//    }
 
     public Cursor getAllData() {
         SQLiteDatabase db = this.getReadableDatabase();
@@ -108,14 +89,12 @@ public class TagPhotoDbHelper extends SQLiteOpenHelper {
      * @return true if update went successfuly, false if there was a problem and more than one row
      * was changed
      */
-    public boolean updateTags(String fileName, String[] keywords) {
+    public boolean updateTags(String fileName, String keywords) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
         values.put(COL2, fileName); // task name
-        for (int i = 0; i < keywords.length; i++) { // Add keywords to corresponding column
-            values.put("keyword" + Integer.toString((i + 1)), keywords[i]);
-        }
+        values.put(COL3, keywords);
 
         int rowsAffected = db.update(TABLE_NAME, values, "file_name = ?", new String[]{fileName});
 
@@ -136,29 +115,48 @@ public class TagPhotoDbHelper extends SQLiteOpenHelper {
 
     /**
      * This method returns the tags for a specific file
-     *
      * @param fileName - The file to find the tags for
      * @return a String array of all the tags for the specific file
      */
-    public String[] getTags(String fileName) {
+    public String getTags(String fileName) {
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor row = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + COL2 + " = ?",
                 new String[]{fileName});
 
-        ArrayList<String> keyArray = new ArrayList<>();
+        if (row.getCount() == 0) {
+            return null;
+        }
+
+        row.moveToNext();
+
+        db.close();
+
+        return row.getString(2);
+
+    }
+
+
+    public ArrayList<String> getFilesWithTag (String[] keywords) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String sql = "SELECT * FROM " + TABLE_NAME + " WHERE " + COL3 + " = ?";
+
+        Cursor row = db.rawQuery(sql, keywords);
 
         if (row.getCount() == 0) {
             return null;
         }
 
-        while (row.moveToNext()) { // TODO fix ALL variable names where necessary (fixed values)
-            for (int i = 2; i < 12 && (!row.getString(i).isEmpty()); i++) { // TODO notice variables here as well
-                keyArray.add(row.getString(i));
-            }
+        ArrayList<String> files = new ArrayList<>();
+
+        while (row.moveToNext()) {
+            files.add(row.getString(1));  // TODO fix variable name
         }
 
-        return (String[]) keyArray.toArray();
+        db.close();
+
+        return files;
 
     }
 
